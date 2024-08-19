53ed22f772998190bcce95a46e21f83db670e55f
ikechiamaka Ebenezer <66757712+ikechiamaka@users.noreply.github.com>
2024-08-19 21:55:05 +0100

    forms.py

--- app/forms.py
+++ app/forms.py
@@ -11,33 +11,33 @@ from flask_wtf.file import FileField, FileAllowed
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField, IntegerField, BooleanField, FileField
 from wtforms.validators import DataRequired, Optional
-from flask_wtf.file import FileAllowed
 
 
-class RegistrationForm(FlaskForm):
-    forename = StringField('Forename', validators=[DataRequired(), Length(min=2, max=20)])
-    surname = StringField('Surname', validators=[DataRequired(), Length(min=2, max=20)])
-    date_of_birth = DateField('Date Of Birth', format='%Y-%m-%d', validators=[DataRequired()])
-    telephone = StringField('Telephone', validators=[DataRequired(), Length(min=10, max=15)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    confirm_email = StringField('Confirm Email', validators=[DataRequired(), Email(), EqualTo('email')])
-    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
-    services_interested_in = StringField('Services interested In', validators=[DataRequired()])
-    preferred_contact_method = SelectField('Preferred Contact Method', choices=[('Email', 'Email'), ('Phone', 'Phone')], validators=[DataRequired()])
-    membership = SelectField('Membership', choices=[('Standard', 'Standard'), ('Premium', 'Premium')], validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Sign Up')
 
-    def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
-        if user:
-            raise ValidationError('That username is taken. Please choose a different one.')
+# class RegistrationForm(FlaskForm):
+#     forename = StringField('Forename', validators=[DataRequired(), Length(min=2, max=20)])
+#     surname = StringField('Surname', validators=[DataRequired(), Length(min=2, max=20)])
+#     date_of_birth = DateField('Date Of Birth', format='%Y-%m-%d', validators=[DataRequired()])
+#     telephone = StringField('Telephone', validators=[DataRequired(), Length(min=10, max=15)])
+#     email = StringField('Email', validators=[DataRequired(), Email()])
+#     confirm_email = StringField('Confirm Email', validators=[DataRequired(), Email(), EqualTo('email')])
+#     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+#     services_interested_in = StringField('Services interested In', validators=[DataRequired()])
+#     preferred_contact_method = SelectField('Preferred Contact Method', choices=[('Email', 'Email'), ('Phone', 'Phone')], validators=[DataRequired()])
+#     membership = SelectField('Membership', choices=[('Standard', 'Standard'), ('Premium', 'Premium')], validators=[DataRequired()])
+#     password = PasswordField('Password', validators=[DataRequired()])
+#     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+#     submit = SubmitField('Sign Up')
 
-    def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
-        if user:
-            raise ValidationError('That email is taken. Please choose a different one.')
+#     def validate_username(self, username):
+#         user = User.query.filter_by(username=username.data).first()
+#         if user:
+#             raise ValidationError('That username is taken. Please choose a different one.')
+
+#     def validate_email(self, email):
+#         user = User.query.filter_by(email=email.data).first()
+#         if user:
+#             raise ValidationError('That email is taken. Please choose a different one.')
 
 
 class LoginForm(FlaskForm):
